b3c60321d8943772cabf15b7a8df4d10f5fe93e7
DontsaClaudia <92451620+DontsaClaudia@users.noreply.github.com>
2022-11-18 11:59:15 +0100

    Type de duree de l'annonce cest en date

--- Tables/Annonces.sql
+++ Tables/Annonces.sql
@@ -1,7 +1,7 @@
 CREATE TABLE Annonces (
     id_annonces integer(10) PRIMARY KEY NOT NULL,
     date_publication DATE,
-    duree TIME,
+    duree DATE,
     Pressingsid_pressings VARCHAR(255),
     Utilisateurid_utilisateur VARCHAR(255)
 );
