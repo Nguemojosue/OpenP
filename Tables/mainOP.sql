bbdee8fe3184b65874c3215625e819760797ba24
Dahlia Djoumessi <118018373+Dahlia09@users.noreply.github.com>
2022-11-19 23:59:50 +0100

    update des tables

--- Tables/mainOP.sql
+++ Tables/mainOP.sql
@@ -27,17 +27,43 @@ PROMPT******************** create table promos**********************************
 PROMPT******************** create table services************************************
 @/creation_tables/table_services
 
-
-
 PROMPT******************** create table type_services************************************
 @/creation_tables/table_type_services
 
+PROMPT******************** create table annonces************************************
+@/creation_tables/table_annonces
 
+PROMPT******************** create table adresses************************************
+@/creation_tables/table_adresses
 
-PROMPT******************** create table valeurattributs************************************
-@/creation_tables/table_valeurattributs
+PROMPT******************** create table utilisateurs************************************
+@/creation_tables/table_utilisateurs
 
+PROMPT******************** create table personnes************************************
+@/creation_tables/table_personnes
 
+PROMPT******************** create table roles************************************
+@/creation_tables/table_roles
+
+PROMPT******************** create table privileges************************************
+@/creation_tables/table_privileges
+
+PROMPT******************** create table pressings************************************
+@/creation_tables/table_pressings
+
+PROMPT******************** create table besoinclient************************************
+@/creation_tables/table_besoinclient
+
+PROMPT******************** create table client************************************
+@/creation_tables/table_client
+PROMPT******************** create table personnephysique************************************
+@/creation_tables/table_personnesphysique
+
+PROMPT******************** create table valeurattribut************************************
+@/creation_tables/table_valeurattribut
+
+PROMPT******************** create table typelinges************************************
+@/creation_tables/table_typelinges
 
 
 --add constraints of tables
