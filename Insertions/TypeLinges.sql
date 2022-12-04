51b6271e3e73c2887fb8f1b61b5d2309fa729e8c
Dahlia Djoumessi <118018373+Dahlia09@users.noreply.github.com>
2022-12-04 14:22:15 +0100

    Update TypeLinges.sql

--- Insertions/TypeLinges.sql
+++ Insertions/TypeLinges.sql
@@ -25,10 +25,22 @@ INSERT INTO TypeLinges (id_typeLinges, nature, matiere,LingeClientid_LingeClient
 VALUES(id_typeLinges_seq.nextval,'drap', 'cuire', '46');
 
 INSERT INTO TypeLinges (id_typeLinges, nature, matiere,LingeClientid_LingeClient)
-VALUES(id_typeLinges_seq.nextval,'tapis', 'cuire', '46');
+VALUES(id_typeLinges_seq.nextval,'tapis', 'coton', '46');
 
 INSERT INTO TypeLinges (id_typeLinges, nature, matiere,LingeClientid_LingeClient)
-VALUES(id_typeLinges_seq.nextval,'coussin', 'cuire', '46');
+VALUES(id_typeLinges_seq.nextval,'coussin', 'coton', '46');
 
 INSERT INTO TypeLinges (id_typeLinges, nature, matiere,LingeClientid_LingeClient)
 VALUES(id_typeLinges_seq.nextval,'jupe', 'cuire', '46');
+
+INSERT INTO TypeLinges (id_typeLinges, nature, matiere,LingeClientid_LingeClient)
+VALUES(id_typeLinges_seq.nextval,'rideau', 'tissu', '46');
+
+INSERT INTO TypeLinges (id_typeLinges, nature, matiere,LingeClientid_LingeClient)
+VALUES(id_typeLinges_seq.nextval,'robe', 'pagne', '46');
+
+INSERT INTO TypeLinges (id_typeLinges, nature, matiere,LingeClientid_LingeClient)
+VALUES(id_typeLinges_seq.nextval,'lingerie', 'coton', '46');
+
+INSERT INTO TypeLinges (id_typeLinges, nature, matiere,LingeClientid_LingeClient)
+VALUES(id_typeLinges_seq.nextval,'jupe', 'polystyrene', '46');
