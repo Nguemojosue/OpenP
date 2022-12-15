5b8cadb31b7220ad85ebd7bfe99af60b90149ad1
DontsaClaudia <92451620+DontsaClaudia@users.noreply.github.com>
2022-12-15 11:37:26 +0100

    insertions table type_services

---
+++ Insertions/type_services.sql
@@ -0,0 +1,19 @@
+PROMPT ********** INSERTIONS DE LA TABLE TYPE_SERVICES *******************
+
+INSERT INTO type_services (type_service_id,nature)
+VALUES (1, "lavage à sec");
+
+INSERT INTO type_services (type_service_id,nature)
+VALUES (2, "lavage à eau simple");
+
+INSERT INTO type_services (type_service_id,nature)
+VALUES (3, "lavage eau avec amidon");
+
+INSERT INTO type_services (type_service_id,nature)
+VALUES (4, "repassage");
+
+INSERT INTO type_services (type_service_id,nature)
+VALUES (5, "defroissage");
+
+INSERT INTO type_services (type_service_id,nature)
+VALUES (6, "teinture");
