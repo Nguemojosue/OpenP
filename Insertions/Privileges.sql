53b56025144d40adaa60d1769ac27638f7ab14ee
DontsaClaudia <92451620+DontsaClaudia@users.noreply.github.com>
2022-12-15 12:25:56 +0100

    insertions table privileges

---
+++ Insertions/Privileges.sql
@@ -0,0 +1,28 @@
+PROMPT********** INSERTIONS DE LA TABLE PRIVILEGES ***********
+
+INSERT INTO (id_privileges,nature,descriptions,RolesUtilisateurid_utilisateur,RolesPressingsid_pressings)
+VALUES ("21","admin","l'utilisateur peut effectuer toute opération","21","21");
+
+INSERT INTO (id_privileges,nature,descriptions,RolesUtilisateurid_utilisateur,RolesPressingsid_pressings)
+VALUES ("22","employe","gerer les clients ","22","22");
+
+INSERT INTO (id_privileges,nature,descriptions,RolesUtilisateurid_utilisateur,RolesPressingsid_pressings)
+VALUES ("23","gerant","l utilisateur gere le pressing","23","23");
+
+INSERT INTO (id_privileges,nature,descriptions,RolesUtilisateurid_utilisateur,RolesPressingsid_pressings)
+VALUES ("24","client","consulte les offres
+                        creer le besoin
+                        fait des echenges avec le gerant
+                          ",                                "24","24");
+
+INSERT INTO (id_privileges,nature,descriptions,RolesUtilisateurid_utilisateur,RolesPressingsid_pressings)
+VALUES ("25","internaute","l'utilisateur visite le pressing les offres ","25","25");
+
+INSERT INTO (id_privileges,nature,descriptions,RolesUtilisateurid_utilisateur,RolesPressingsid_pressings)
+VALUES ("26","livreur","l'utilisateur livre les commandes aux differents clients","26","26");
+
+INSERT INTO (id_privileges,nature,descriptions,RolesUtilisateurid_utilisateur,RolesPressingsid_pressings)
+VALUES ("27","directeur","l'utilisateur peut effectuer toute opération","27","27");
+
+INSERT INTO (id_privileges,nature,descriptions,RolesUtilisateurid_utilisateur,RolesPressingsid_pressings)
+VALUES ("28","caissie","l'utilisateur gere la caisse","28","28");
