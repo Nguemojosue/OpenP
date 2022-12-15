dcaeaba3826e17cb08619e13754b24baf77c1f22
DontsaClaudia <92451620+DontsaClaudia@users.noreply.github.com>
2022-12-15 11:49:41 +0100

    Update Privileges.sql
    j'ai ajouter s a description par ce que sans s est un mot cle

--- Tables/Privileges.sql
+++ Tables/Privileges.sql
@@ -3,7 +3,7 @@ Prompt ******  Creating Privileges table ....
 CREATE TABLE Privileges (
     id_privileges VARCHAR2(255) PRIMARY KEY NOT NULL,
     nature VARCHAR2(255),
-    description VARCHAR2(255),
+    descriptions VARCHAR2(255),
     RolesUtilisateurid_utilisateur VARCHAR2(255) NOT NULL,
     RolesPressingsid_pressings VARCHAR2(255) NOT  NULL
 );
