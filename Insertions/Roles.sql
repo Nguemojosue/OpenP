a16e106b2935fdae1b2d47f537141880faab1fd5
DontsaClaudia <92451620+DontsaClaudia@users.noreply.github.com>
2022-12-15 11:36:33 +0100

    insertions table roles

---
+++ Insertions/Roles.sql
@@ -0,0 +1,89 @@
+PROMPT ****** INSERTIONS DE LA TABLE ROLES **********
+
+INSERT INTO Roles (Utilisateurid_utilisateur,Pressingsid_pressings,type_role)
+VALUES (21,21,"direteur pressing");
+
+INSERT INTO Roles (Utilisateurid_utilisateur,Pressingsid_pressings,type_role)
+VALUES (22,22,"employe");
+
+INSERT INTO Roles (Utilisateurid_utilisateur,Pressingsid_pressings,type_role)
+VALUES (23,23,"gerant de pressing");
+
+INSERT INTO Roles (Utilisateurid_utilisateur,Pressingsid_pressings,type_role)
+VALUES (24,24,"employe");
+
+INSERT INTO Roles (Utilisateurid_utilisateur,Pressingsid_pressings,type_role)
+VALUES (25,25,"employe");
+
+INSERT INTO Roles (Utilisateurid_utilisateur,Pressingsid_pressings,type_role)
+VALUES (26,26,"livreur");
+
+INSERT INTO Roles (Utilisateurid_utilisateur,Pressingsid_pressings,type_role)
+VALUES (27,27,"livreur");
+
+INSERT INTO Roles (Utilisateurid_utilisateur,Pressingsid_pressings,type_role)
+VALUES (28,28,"direteur pressing");
+
+INSERT INTO Roles (Utilisateurid_utilisateur,Pressingsid_pressings,type_role)
+VALUES (29,29,"gerant pressing");
+
+INSERT INTO Roles (Utilisateurid_utilisateur,Pressingsid_pressings,type_role)
+VALUES (30,30,"employe");
+
+INSERT INTO Roles (Utilisateurid_utilisateur,Pressingsid_pressings,type_role)
+VALUES (31,31,"employe");
+
+INSERT INTO Roles (Utilisateurid_utilisateur,Pressingsid_pressings,type_role)
+VALUES (32,32,"employe");
+
+INSERT INTO Roles (Utilisateurid_utilisateur,Pressingsid_pressings,type_role)
+VALUES (33,33,"livreur");
+
+INSERT INTO Roles (Utilisateurid_utilisateur,Pressingsid_pressings,type_role)
+VALUES (34,34,"livreur");
+
+INSERT INTO Roles (Utilisateurid_utilisateur,Pressingsid_pressings,type_role)
+VALUES (35,35,"direteur pressing");
+
+INSERT INTO Roles (Utilisateurid_utilisateur,Pressingsid_pressings,type_role)
+VALUES (36,36,"gerant pressing");
+
+INSERT INTO Roles (Utilisateurid_utilisateur,Pressingsid_pressings,type_role)
+VALUES (37,37,"employe");
+
+INSERT INTO Roles (Utilisateurid_utilisateur,Pressingsid_pressings,type_role)
+VALUES (38,38,"employe");
+
+INSERT INTO Roles (Utilisateurid_utilisateur,Pressingsid_pressings,type_role)
+VALUES (39,39,"employe");
+
+INSERT INTO Roles (Utilisateurid_utilisateur,Pressingsid_pressings,type_role)
+VALUES (40,40,"livreur");
+
+INSERT INTO Roles (Utilisateurid_utilisateur,Pressingsid_pressings,type_role)
+VALUES (41,41,"livreur");
+
+INSERT INTO Roles (Utilisateurid_utilisateur,Pressingsid_pressings,type_role)
+VALUES (42,42,"direteur pressing");
+
+INSERT INTO Roles (Utilisateurid_utilisateur,Pressingsid_pressings,type_role)
+VALUES (43,43,"gerant pressing");
+
+INSERT INTO Roles (Utilisateurid_utilisateur,Pressingsid_pressings,type_role)
+VALUES (44,44,"employe");
+
+INSERT INTO Roles (Utilisateurid_utilisateur,Pressingsid_pressings,type_role)
+VALUES (45,45,"employe");
+
+INSERT INTO Roles (Utilisateurid_utilisateur,Pressingsid_pressings,type_role)
+VALUES (46,46,"employe");
+
+INSERT INTO Roles (Utilisateurid_utilisateur,Pressingsid_pressings,type_role)
+VALUES (47,47,"livreur");
+
+INSERT INTO Roles (Utilisateurid_utilisateur,Pressingsid_pressings,type_role)
+VALUES (48,48,"livreur");
+
+INSERT INTO Roles (Utilisateurid_utilisateur,Pressingsid_pressings,type_role)
+VALUES (49,49,"gerant pressing");
+
